3a434c1710c9383175a983c0eef4b8e9b26c7a89
Isma1306 <isma1306@gmail.com>
2022-07-14 15:18:42 +0200

    Generated missing components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
+import { InfoCardComponent } from './header/info-card/info-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchBarComponent,
+    InfoCardComponent
   ],
   imports: [
     BrowserModule
