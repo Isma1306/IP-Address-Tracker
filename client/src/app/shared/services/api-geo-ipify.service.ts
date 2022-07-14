2e91d6f31c0955bc64f67bfdbd823eb7ab38fc00
Isma1306 <isma1306@gmail.com>
2022-07-14 18:26:33 +0200

    Added mock data to api service

--- client/src/app/shared/services/api-geo-ipify.service.ts
+++ client/src/app/shared/services/api-geo-ipify.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class APIGeoIpifyService {
 
   constructor() { }
+
+
+  getLocation(address: string): Observable<any> {
+    const response = {
+      "ip": "8.8.8.8",
+      "location": {
+        "country": "US",
+        "region": "California",
+        "city": "Mountain View",
+        "lat": 37.40599,
+        "lng": -122.078514,
+        "postalCode": "94043",
+        "timezone": "-07:00",
+        "geonameId": 5375481
+      },
+      "domains": [
+        "0d2.net",
+        "003725.com",
+        "0f6.b0094c.cn",
+        "007515.com",
+        "0guhi.jocose.cn"
+      ],
+      "as": {
+        "asn": 15169,
+        "name": "Google LLC",
+        "route": "8.8.8.0/24",
+        "domain": "https://about.google/intl/en/",
+        "type": "Content"
+      },
+      "isp": "Google LLC"
+    };
+
+    const res = of(response);
+    return res;
+
+  }
 }
